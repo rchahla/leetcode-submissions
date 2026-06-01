74855a6d9c0eed4ce23d0c3b4ceda991459b6a64
Riad Chahla <145374837+rchahla@users.noreply.github.com>
2026-06-01 14:53:00 -0400

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+    if (strs.length === 1) return [[...strs]];
+
+  const map = new Map();
+
+  for (const word of strs) {
+    const key = word.split("").sort().join("");
+
+    if (!map.has(key)) map.set(key, []);
+
+    map.get(key).push(word);
+  }
+
+  return Array.from(map.values());
+    }
+}
