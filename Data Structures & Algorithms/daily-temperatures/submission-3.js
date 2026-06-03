e31c825103e4570b8d0673688591f37d04f7e4e0
Riad Chahla <145374837+rchahla@users.noreply.github.com>
2026-06-03 17:17:17 -0400

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+
+        let stack = [];
+
+        let result = new Array(temperatures.length).fill(0);
+
+        for (let temp in temperatures){
+            if (stack.length === 0 ) {
+                stack.push(temp);
+                continue;
+            } 
+
+            let prevTemp = temperatures[stack[stack.length - 1]];
+            let currTemp = temperatures[temp];
+
+            while (currTemp > prevTemp){
+
+                let properIndex = stack.pop();
+
+                let valIndex = temp - properIndex;
+
+                result[properIndex] = valIndex;
+
+                if(stack.length ===0 )break;
+
+                prevTemp= temperatures[stack[stack.length - 1]];
+
+            }
+            stack.push(temp)
+        }
+
+        return result;
+    }
+}
