1bcc6e5f9447479def990defe26c4c7ce5a6e7d3
Riad Chahla <145374837+rchahla@users.noreply.github.com>
2026-06-13 02:59:49 -0400

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.js
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+
+        let left=0;
+
+        let right= nums.length - 1;
+
+        while (left <=right){
+            let midpoint = Math.floor((left + right)/2);
+
+            if (nums[midpoint] < target){
+                left = midpoint + 1;
+                // if (nums[left] === target) return left
+            } else if(nums[midpoint] > target){
+                right = midpoint - 1;
+                // if (nums[right] === target) return right
+            }else {
+                return midpoint;
+            }
+        }
+
+        return -1;
+    }
+}
+
+
